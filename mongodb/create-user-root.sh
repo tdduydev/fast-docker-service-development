4a982ee2a92d4aa06b8635f7fe8f795687cc9c0e
METQUA <dauemail@gmail.com>
2022-08-05 14:25:39 +0700

    fix create user

--- mongodb/create-user-root.sh
+++ mongodb/create-user-root.sh
@@ -1,15 +1,1 @@
-docker exec mongo1 
-mongo <<EOF
-db.createUser(
-        {
-            user: "userRoot",
-            pwd: "RmRoY0AxMjM=",
-            roles: [
-                {
-                    role: "readWrite",
-                    db: "admin"
-                }
-            ]
-        }
-);
-EOF
+docker exec mongo1 /scripts/create-user-root.sh
